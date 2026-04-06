7631883a539e25b024b3f1874c56c29760f4b640
tobese <tommy.bergstrand@gmail.com>
2026-04-06 21:55:00 +0200

    feat: add View Pipeline Runs button linking to GitHub Actions

--- new-bank/multi-client/InternetBankCalculator/InternetBankCalculator/SystemDesignPage.xaml.cs
+++ new-bank/multi-client/InternetBankCalculator/InternetBankCalculator/SystemDesignPage.xaml.cs
@@ -20,4 +20,11 @@ public sealed partial class SystemDesignPage : Page
         Uno.Foundation.WebAssemblyRuntime.InvokeJS("window.open('http://localhost:9090', '_blank')");
 #endif
     }
+
+    private void BtnGitHubActions_Click(object sender, RoutedEventArgs e)
+    {
+#if __WASM__
+        Uno.Foundation.WebAssemblyRuntime.InvokeJS("window.open('https://github.com/tobese/internet-bank-case-study/actions', '_blank')");
+#endif
+    }
 }
