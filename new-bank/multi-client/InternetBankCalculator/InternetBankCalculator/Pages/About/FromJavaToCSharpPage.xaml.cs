1edc9249dcb8ba3aaaf85f397d45ab34784ffd96
tobese <tommy.bergstrand@gmail.com>
2026-04-12 20:12:32 +0200

    fix: working navigation and back button for About pages (checkpoint)

--- new-bank/multi-client/InternetBankCalculator/InternetBankCalculator/Pages/About/FromJavaToCSharpPage.xaml.cs
+++ new-bank/multi-client/InternetBankCalculator/InternetBankCalculator/Pages/About/FromJavaToCSharpPage.xaml.cs
@@ -7,5 +7,21 @@ public sealed partial class FromJavaToCSharpPage : Page
     public FromJavaToCSharpPage()
     {
         this.InitializeComponent();
+#if WINDOWS_UWP || HAS_UNO_WINUI
+        var navManager = Windows.UI.Core.SystemNavigationManager.GetForCurrentView();
+        navManager.BackRequested += OnBackRequested;
+#endif
     }
+
+#if WINDOWS_UWP || HAS_UNO_WINUI
+    private void OnBackRequested(object sender, Windows.UI.Core.BackRequestedEventArgs e)
+    {
+        // Mark event as handled and navigate as needed
+        e.Handled = true;
+        if (Frame.CanGoBack)
+        {
+            Frame.GoBack();
+        }
+    }
+#endif
 }
